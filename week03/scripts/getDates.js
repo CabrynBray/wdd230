128fcd34c6eb89fcfe064e1dc017b94b2c60843e
CabrynBray <cabrynbrayza@gmail.comgit config --global user.name>
2023-11-14 13:12:41 +0200

    lazy load completed

---
+++ week03/scripts/getDates.js
@@ -0,0 +1,23 @@
+
+// Function to set the copyright year dynamically
+function setCopyrightYear() {
+    const currentYear = new Date().getFullYear();
+    const copyrightElement = document.querySelector('#copyright-year');
+    if (copyrightElement) {
+        copyrightElement.textContent = currentYear;
+    }
+
+}
+
+// Function to display the last modified date
+function setLastModified() {
+    const lastModified = new Date(document.lastModified);
+    const lastModifiedElement = document.querySelector('#lastModified');
+    if (lastModifiedElement) {
+        lastModifiedElement.textContent = lastModified;
+    }
+}
+
+// Call the functions to update the elements
+setCopyrightYear();
+setLastModified();
