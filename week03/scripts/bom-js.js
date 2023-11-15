89677f8fbf7f4c6e683cd773a85eb446c6503805
CabrynBray <cabrynbrayza@gmail.comgit config --global user.name>
2023-11-15 16:18:49 +0200

    bom completed

---
+++ week03/scripts/bom-js.js
@@ -0,0 +1,77 @@
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('#list');
+
+let chaptersArray = getChapterList() || [];
+
+chaptersArray.forEach(chapter => {
+    displayList(chapter);
+})
+
+button.addEventListener('click', () => {
+    if (input.value != '') {
+        // call displayList with the input.value argument
+        displayList(input.value);
+
+        // push the input.value into the chaptersArray,
+        chaptersArray.push(displayList);
+
+        // update the localStorage with the new array by calling a function named setChapterList
+        setChapterList();
+
+        // set the input.value to nothing,
+        input.value = '';
+        // set the focus back to the input.
+        input.focus();
+
+
+    }
+
+})
+
+function displayList(item) {
+    // create a li element & create a delete button
+    const li = document.createElement('li');
+    const deleteButton = document.createElement('button');
+
+    // populate the li elements textContent with the input value
+    li.textContent = item;
+
+    // populate the button textContent with a ❌
+    deleteButton.textContent = '❌';
+    deleteButton.classList.add('delete'); // this references the CSS rule .delete{width:fit-content;} to size the delete button
+
+    // append the li element with the delete button
+    li.append(deleteButton);
+    // append the li element to the unordered list in your HTML
+    list.append(li);
+
+    // add an event listener to the delete button that removes the li element when clicked
+    deleteButton.addEventListener('click', function () {
+        list.removeChild(li);
+        deleteChapter(li.textContent); // note this new function that is needed to remove the chapter from the array and localStorage.
+        input.focus();
+    })
+
+    // send the focus to the input element
+    input.focus();
+
+    // change the input value to nothing or the empty string to clean up the interface for the user
+    input.value = '';
+
+}
+
+function setChapterList() {
+    localStorage.setItem('myFavBOMList', JSON.stringify(chaptersArray));
+}
+
+function getChapterList() {
+    return JSON.parse(localStorage.getItem('myFavBOMList'));
+}
+
+function deleteChapter(chapter) {
+    chapter = chapter.slice(0, chapter.length - 1); // this slices off the last character
+    chaptersArray = chaptersArray.filter((item) => item !== chapter);
+    setChapterList();
+}
+
