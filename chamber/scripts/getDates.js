6e520e00847d966f72862f85efdc45fe238a16ea
CabrynBray <cabrynbrayza@gmail.comgit config --global user.name>
2023-11-17 11:17:20 +0200

    fixed error and added css to chamber

--- chamber/scripts/getDates.js
+++ chamber/scripts/getDates.js
@@ -87,3 +87,24 @@ modeButton.addEventListener("click", () => {
     }
 });
 */
+
+/* Discover */
+// Number of Visits
+// 1️⃣ Initialize display element variable
+const visitsDisplay = document.querySelector(".num-visits");
+
+// 2️⃣ Get the stored VALUE for the numVisits-ls KEY in localStorage if it exists. If the numVisits KEY is missing, then assign 0 to the numVisits variable.
+let numVisits = Number(window.localStorage.getItem("number-of-visits")) || 0;
+
+// 3️⃣ Determine if this is the first visit or display the number of visits. We wrote this example backwards in order for you to think deeply about the logic.
+if (numVisits !== 0) {
+    visitsDisplay.textContent = numVisits;
+} else {
+    visitsDisplay.textContent = `Welcome! Let us know if you have any questions.`;
+}
+
+// 4️⃣ increment the number of visits by one.
+numVisits++;
+
+// 5️⃣ store the new visit total into localStorage, key=numVisits-ls
+localStorage.setItem("number-of-visits", numVisits);
