bc0a45fafdae34124b5fe9275c13cb8e5cad19a7
CabrynBray <cabrynbrayza@gmail.comgit config --global user.name>
2023-11-23 15:24:43 +0200

    added time stamp

--- chamber/scripts/getDates.js
+++ chamber/scripts/getDates.js
@@ -127,3 +127,11 @@ if (lastVisitDate !== 0) {
 }
 
 localStorage.setItem("lastVisitDate", Date.now());
+
+
+// --------------------- Time stamp -------------------------
+function setTimestamp() {
+    var timestampField = document.getElementById("timestamp");
+    var currentDate = new Date();
+    timestampField.value = currentDate.toISOString(); // Set the timestamp in ISO format
+}
