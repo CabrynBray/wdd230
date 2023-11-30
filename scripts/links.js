6f5e3c9ff3093531ff75457d411df58ef76d940d
CabrynBray <cabrynbrayza@gmail.comgit config --global user.name>
2023-11-30 14:50:06 +0200

    completed team activity

--- scripts/links.js
+++ scripts/links.js
@@ -1,20 +1,44 @@
 const baseURL = "https://cabrynbray.github.io/wdd230/";
 const linksURL = "https://cabrynbray.github.io/wdd230/data/links.json";
 
-const weeksList = document.querySelector(".weeks")
+const activityList = document.getElementById("activity-list");
 
 async function getLinks() {
-    const response = await fetch(linksURL);
-    const data = await response.json();
-    console.log(data);
-    displayLinks(data);
+    try {
+        const response = await fetch(linksURL);
+        const data = await response.json();
+        // console.log(data);
+        displayLinks(data.weeks);
+    } catch (error) {
+        console.error("Error fetching links data:", error);
+    }
 }
 
 function displayLinks(weeks) {
+
+
     weeks.forEach((week) => {
+        const weekElement = document.createElement("li");
+        weekElement.textContent = week.week;
 
+        const linksList = document.createElement("ul");
 
-    })
-}
+        // Loop through each link in the week
+        week.links.forEach((link) => {
+            const li = document.createElement("li");
+            const a = document.createElement("a");
+
+            a.href = baseURL + link.url;
+            a.textContent = link.title;
+
+            li.appendChild(a);
+            linksList.appendChild(li);
+        });
+
+        weekElement.appendChild(linksList);
+        activityList.appendChild(weekElement);
+
+    });
+};
 
 getLinks();
