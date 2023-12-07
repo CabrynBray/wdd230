6970c0a17a2664af309e109e254e50a7000c0f0a
CabrynBray <cabrynbrayza@gmail.comgit config --global user.name>
2023-12-07 13:27:59 +0200

    fixed banner

--- chamber/scripts/banner.js
+++ chamber/scripts/banner.js
@@ -1,11 +1,17 @@
 const today = new Date;
-const banner = document.querySelector("#banner");
-const bannerBtn = document.querySelector("#close-banner");
+const banButton = document.querySelector('#close-banner');
+const banner = document.querySelector('#banner');
 
-if (today.getDate() >= 0 && today.getDate() <= 2) {
+// console.log(`Today = ${today.getDay()}`);
+
+if (today.getDay() == 0) {
+    banner.style.display = 'none';
+} else if (today.getDay() > 3) {
+    banner.style.display = 'none';
+} else {
     banner.style.display = 'flex';
-}
+};
 
-bannerBtn.addEventListener('click', () => {
+banButton.addEventListener('click', () => {
     banner.style.display = 'none';
 });
