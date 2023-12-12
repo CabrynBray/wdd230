238b8567bdb1483aa90be96a39afe1ce1a281330
CabrynBray <cabrynbrayza@gmail.comgit config --global user.name>
2023-12-12 10:32:49 +0200

    added hidden time stamp

--- chamber/scripts/join.js
+++ chamber/scripts/join.js
@@ -1,8 +1,4 @@
 // --------------------- Time stamp -------------------------
-function setTimestamp() {
-    var timestampField = document.getElementById("timestamp");
-    var currentDate = new Date();
-    timestampField.value = currentDate.toISOString(); // Set the timestamp in ISO format
-}
+const timestamp = document.getElementById("timestamp");
 
-setTimestamp()
+timestamp.value = new Date().toISOString();
